1546e4c91a01011690c554b2b33fcd192fb32ef7
Andrei Pronichev <andrei.pronichev@jetbrains.com>
2025-03-25 17:53:01 +0200

    Add TeamCity CI configuration with main build setup

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,8 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.triggers.schedule
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -24,5 +28,64 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 
 version = "2024.12"
 
+object SpringFrameworkVcs : GitVcsRoot({
+    name = "Spring Framework"
+    url = "https://github.com/spring-projects/spring-framework.git"
+    branch = "refs/heads/main"
+    branchSpec = "+:*"
+    checkoutMode = CheckoutMode.AUTO
+})
+
+object MainCIBuild : BuildType({
+    name = "Main CI Build"
+    description = "Main CI build running on multiple Java versions"
+    
+    vcs {
+        root(SpringFrameworkVcs)
+    }
+
+    params {
+        param("env.JAVA_HOME", "%env.JDK_17%")
+        param("kotlinVersion", "2.1.20")
+        param("version", "7.0.0-SNAPSHOT")
+    }
+
+    steps {
+        gradle {
+            name = "Build and Test"
+            tasks = "check antora"
+            gradleWrapperPath = "gradlew"
+            buildFile = "build.gradle"
+            gradleParams = "-Xmx2048m --parallel"
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = "+:*"
+        }
+        schedule {
+            schedulingPolicy = daily {
+                hour = 9
+                minute = 30
+            }
+            branchFilter = "+:*"
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+        contains("teamcity.agent.jvm.version", "17")
+    }
+
+    artifactRules = """
+        +:build/reports/** => build-reports
+        +:build/test-results/** => test-results
+        +:build/libs/** => artifacts
+    """.trimIndent()
+})
+
 project {
+    vcsRoot(SpringFrameworkVcs)
+    buildType(MainCIBuild)
 }
